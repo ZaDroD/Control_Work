b9168ef0a071fda15db36206ad3840e8da2e40c6
Evgeniy <zadrod93@yandex.ru>
2022-04-03 17:46:47 +0300

    Пишем решение

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,6 +4,29 @@
 // длинна которых меньше либо равна 3 символа.
 
 
-string[] firstArray = { "One", "Repo", "Double", "Fin", "Finish" };
+string[] firstArray = { "One", "Repo", "Double", "123", "3345", "Fin", "Finish" };
 Console.WriteLine("Задан массив строк:");
 PrintArray(firstArray);
+string[] newArray = GetNewArray(firstArray);
+string[] GetNewArray(string[] firstArray)
+{
+    string[] newArray = new string[firstArray.Length];
+    int size = 0;
+    for (var i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            newArray[size] = firstArray[i];
+            size++;
+        }
+    }
+    Array.Resize(ref newArray, size);
+    return newArray;
+}
+void PrintArray(string[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write(item);
+    }
+}
